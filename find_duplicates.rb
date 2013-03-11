d8331600e4e49378a9ca04286773d7eca5a9e898
Karl Chu <kchu13@gmail.com>
2013-03-11 21:03:29 +1100

    added some optimization to compare the files by size first before calculating the digest

--- find_duplicates.rb
+++ find_duplicates.rb
@@ -1,9 +1,9 @@
 require "digest/md5"
 
 # TODO: Take in the FOLDER_TO_CHECK by command line parameter
-FOLDER_TO_CHECK = "/Volumes/Data/Picasa"
+#FOLDER_TO_CHECK = "/Volumes/Data/Picasa"
 #FOLDER_TO_CHECK = "test_data"
-#FOLDER_TO_CHECK = "/Users/kchu/dev/duplicate_finder/test_data"
+FOLDER_TO_CHECK = "/Users/kchu/dev/duplicate_finder/test_data"
 # TODO: Take in the TO_FOLDER by command line parameter
 TO_FOLDER = "/Volumes/Data/picassa_duplicates"
 # TODO: Take in the SCRIPT_NAME by command line parameter
@@ -11,36 +11,74 @@ SCRIPT_NAME = "do-the-move.sh"
 
 # TODO: Make this cross-platform? (i.e. care a bit more about Windows?)
 
-# Index and create digests for all the files within a directory
-content_hashes = Hash.new {|hash, key| hash[key] = []}
+dir_glob_pattern = "#{FOLDER_TO_CHECK}/**/*"
 
-Dir.glob("#{FOLDER_TO_CHECK}/**/*") do |f|
-  next unless File.file?(f)
+def index_file_size_in_dir(dir_glob_pattern)
+  filesize_hashes = Hash.new { |hash, key| hash[key] = [] }
+  Dir.glob(dir_glob_pattern, File::FNM_CASEFOLD) do |filename|
+    next unless File.file?(filename)
 
-  printf "Computing the MD5 digest for file \"#{f}\": "
-  file_content_digest = Digest::MD5.file(f).hexdigest
-  puts "[#{file_content_digest}]"
-  content_hashes[file_content_digest] << f
+    file_size = File.size(filename)
+    filesize_hashes[file_size] << filename
+  end
+  filesize_hashes
 end
 
-# Identify the files that have the same content hash (i.e. the files are identical)
-duplicate_file_sets = Array.new
-content_hashes.each do |key, value|
-  if value.size > 1
-    duplicate_file_sets << value
+def find_duplicate_files_by_digest(filenames)
+  content_hashes = Hash.new { |hash, key| hash[key] = [] }
+
+  filenames.each do |filename|
+    printf "Computing the MD5 digest for file \"#{filename}\": "
+    file_content_digest = Digest::MD5.file(filename).hexdigest
+    puts "[#{file_content_digest}]"
+    content_hashes[file_content_digest] << filename
+  end
+
+  duplicate_file_sets = Array.new
+
+  content_hashes.values.each do |value|
+    if value.size > 1
+      duplicate_file_sets << value
+    end
+  end
+
+  duplicate_file_sets
+end
+
+def compare_files(x, y)
+  if File.ctime(x) == File.ctime(y)
+    x.casecmp(y)
+  elsif File.ctime(x) < File.ctime(y)
+    -1
+  else
+    1
   end
 end
 
-# Create a new hash that the key is the "original" file name. The values of the hash are arrays of file names
-# that are duplicates of the file identified by the key.
+filesize_hash = index_file_size_in_dir(dir_glob_pattern)
+
+puts 'File sets that have the same size:'
+filesize_hash.values.each do |files_of_same_size|
+  if files_of_same_size.size > 1
+    puts files_of_same_size.join(" <=> ")
+  end
+end
+
 duplicates_hash = Hash.new
-duplicate_file_sets.each do |duplicate_files|
-  duplicate_files.sort! do |x, y|
-    # TODO: Make this sorting more intelligent so that we keep the original file (e.g. IMG_1234.JPG)
-    # rather than the duplicate file (e.g. IMG_1234-001.JPG, or IMG_1234 copy.JPG, etc.)
-    File.basename(x).length - File.basename(y).length
+filesize_hash.values.each do |files_of_same_size|
+  if files_of_same_size.size > 1
+
+    duplicate_file_sets = find_duplicate_files_by_digest(files_of_same_size)
+
+    # Create a new hash that the key is the "original" file name. The values of the hash are arrays of file names
+    # that are duplicates of the file identified by the key.
+    duplicate_file_sets.each do |duplicate_files|
+      duplicate_files.sort! do |x, y|
+        compare_files(x, y)
+      end
+      duplicates_hash[duplicate_files.shift] = duplicate_files
+    end
   end
-  duplicates_hash[duplicate_files.shift] = duplicate_files
 end
 
 bash_script = File.new SCRIPT_NAME, "w"
