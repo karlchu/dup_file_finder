996f016c99916602bc6fd19ff24104298032c423
Karl Chu <kchu13@gmail.com>
2014-12-26 21:30:05 +1100

    Use subject in runtime_parameters_spec.rb

--- spec/runtime_parameters_spec.rb
+++ spec/runtime_parameters_spec.rb
@@ -1,84 +1,84 @@
 require 'rspec'
 require 'minitest/unit'
-require_relative '../lib/runtime_parameters'
+require_relative '../lib/subject'
 
 describe RuntimeParameters do
 
-  let(:runtime_parameters) { RuntimeParameters.new }
+  subject { RuntimeParameters.new }
 
   it 'should not be valid when no argument given' do
-    runtime_parameters.parse([])
+    subject.parse([])
 
-    runtime_parameters.valid?.should == false
+    subject.valid?.should == false
   end
 
   it 'should indicate show help when -h short option given' do
-    runtime_parameters.parse %w(-h)
+    subject.parse %w(-h)
 
-    runtime_parameters.show_help?.should == true
+    subject.show_help?.should == true
   end
 
   it 'should indicate show help when --help long option given' do
-    runtime_parameters.parse %w(--h)
+    subject.parse %w(--h)
 
-    runtime_parameters.show_help?.should == true
+    subject.show_help?.should == true
   end
 
   it 'should parse destination folder option when -d option given' do
-    runtime_parameters.parse %w(-m some/output/folder)
+    subject.parse %w(-m some/output/folder)
 
-    runtime_parameters.destination.should == 'some/output/folder'
+    subject.destination.should == 'some/output/folder'
   end
 
   it 'should parse destination folder option when --dest long option given' do
-    runtime_parameters.parse %w(--move-to some/output/folder)
+    subject.parse %w(--move-to some/output/folder)
 
-    runtime_parameters.destination.should == 'some/output/folder'
+    subject.destination.should == 'some/output/folder'
   end
 
   it 'should parse delete long option' do
-    runtime_parameters.parse %w(--delete some/output/folder)
+    subject.parse %w(--delete some/output/folder)
 
-    runtime_parameters.delete?.should == true
+    subject.delete?.should == true
   end
 
   it 'should parse delete short option' do
-    runtime_parameters.parse %w(--delete some/output/folder)
+    subject.parse %w(--delete some/output/folder)
 
-    runtime_parameters.delete?.should == true
+    subject.delete?.should == true
   end
 
   it 'should parse input folder' do
-    runtime_parameters.parse %w(-m abc path/to/input/folder)
+    subject.parse %w(-m abc path/to/input/folder)
 
-    runtime_parameters.input_folders.should == ['path/to/input/folder']
+    subject.input_folders.should == ['path/to/input/folder']
   end
 
   it 'should be valid when move destination is given' do
-    runtime_parameters.parse %w(-m abc path/to/input/folder)
+    subject.parse %w(-m abc path/to/input/folder)
 
-    runtime_parameters.valid?.should == true
-    runtime_parameters.show_help?.should == false
+    subject.valid?.should == true
+    subject.show_help?.should == false
   end
 
   it 'should be valid when delete short option is given' do
-    runtime_parameters.parse %w(-d path/to/input/folder)
+    subject.parse %w(-d path/to/input/folder)
 
-    runtime_parameters.valid?.should == true
-    runtime_parameters.show_help?.should == false
+    subject.valid?.should == true
+    subject.show_help?.should == false
   end
 
   it 'should be invalid and show help when no input folder given' do
-    runtime_parameters.parse %w(-m abc)
+    subject.parse %w(-m abc)
 
-    runtime_parameters.valid?.should == false
-    runtime_parameters.show_help?.should == true
+    subject.valid?.should == false
+    subject.show_help?.should == true
   end
 
   it 'should be invalid and show help when no destination given' do
-    runtime_parameters.parse %w(path/to/input)
+    subject.parse %w(path/to/input)
 
-    runtime_parameters.valid?.should == false
-    runtime_parameters.show_help?.should == true
+    subject.valid?.should == false
+    subject.show_help?.should == true
   end
 end
