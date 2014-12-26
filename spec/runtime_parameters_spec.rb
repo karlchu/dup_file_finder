e2ad00f724cc0713b859208a0b6cd8cbdad42548
Karl Chu <kchu13@gmail.com>
2014-12-26 21:32:19 +1100

    Put specs in describe block

--- spec/runtime_parameters_spec.rb
+++ spec/runtime_parameters_spec.rb
@@ -6,79 +6,81 @@ describe RuntimeParameters do
 
   subject { RuntimeParameters.new }
 
-  it 'should not be valid when no argument given' do
-    subject.parse([])
+  describe '#parse' do
+    it 'should not be valid when no argument given' do
+      subject.parse([])
 
-    subject.valid?.should == false
-  end
+      subject.valid?.should == false
+    end
 
-  it 'should indicate show help when -h short option given' do
-    subject.parse %w(-h)
+    it 'should indicate show help when -h short option given' do
+      subject.parse %w(-h)
 
-    subject.show_help?.should == true
-  end
+      subject.show_help?.should == true
+    end
 
-  it 'should indicate show help when --help long option given' do
-    subject.parse %w(--h)
+    it 'should indicate show help when --help long option given' do
+      subject.parse %w(--h)
 
-    subject.show_help?.should == true
-  end
+      subject.show_help?.should == true
+    end
 
-  it 'should parse destination folder option when -d option given' do
-    subject.parse %w(-m some/output/folder)
+    it 'should parse destination folder option when -d option given' do
+      subject.parse %w(-m some/output/folder)
 
-    subject.destination.should == 'some/output/folder'
-  end
+      subject.destination.should == 'some/output/folder'
+    end
 
-  it 'should parse destination folder option when --dest long option given' do
-    subject.parse %w(--move-to some/output/folder)
+    it 'should parse destination folder option when --dest long option given' do
+      subject.parse %w(--move-to some/output/folder)
 
-    subject.destination.should == 'some/output/folder'
-  end
+      subject.destination.should == 'some/output/folder'
+    end
 
-  it 'should parse delete long option' do
-    subject.parse %w(--delete some/output/folder)
+    it 'should parse delete long option' do
+      subject.parse %w(--delete some/output/folder)
 
-    subject.delete?.should == true
-  end
+      subject.delete?.should == true
+    end
 
-  it 'should parse delete short option' do
-    subject.parse %w(--delete some/output/folder)
+    it 'should parse delete short option' do
+      subject.parse %w(--delete some/output/folder)
 
-    subject.delete?.should == true
-  end
+      subject.delete?.should == true
+    end
 
-  it 'should parse input folder' do
-    subject.parse %w(-m abc path/to/input/folder)
+    it 'should parse input folder' do
+      subject.parse %w(-m abc path/to/input/folder)
 
-    subject.input_folders.should == ['path/to/input/folder']
-  end
+      subject.input_folders.should == ['path/to/input/folder']
+    end
 
-  it 'should be valid when move destination is given' do
-    subject.parse %w(-m abc path/to/input/folder)
+    it 'should be valid when move destination is given' do
+      subject.parse %w(-m abc path/to/input/folder)
 
-    subject.valid?.should == true
-    subject.show_help?.should == false
-  end
+      subject.valid?.should == true
+      subject.show_help?.should == false
+    end
 
-  it 'should be valid when delete short option is given' do
-    subject.parse %w(-d path/to/input/folder)
+    it 'should be valid when delete short option is given' do
+      subject.parse %w(-d path/to/input/folder)
 
-    subject.valid?.should == true
-    subject.show_help?.should == false
-  end
+      subject.valid?.should == true
+      subject.show_help?.should == false
+    end
 
-  it 'should be invalid and show help when no input folder given' do
-    subject.parse %w(-m abc)
+    it 'should be invalid and show help when no input folder given' do
+      subject.parse %w(-m abc)
 
-    subject.valid?.should == false
-    subject.show_help?.should == true
-  end
+      subject.valid?.should == false
+      subject.show_help?.should == true
+    end
 
-  it 'should be invalid and show help when no destination given' do
-    subject.parse %w(path/to/input)
+    it 'should be invalid and show help when no destination given' do
+      subject.parse %w(path/to/input)
 
-    subject.valid?.should == false
-    subject.show_help?.should == true
+      subject.valid?.should == false
+      subject.show_help?.should == true
+    end
   end
 end
